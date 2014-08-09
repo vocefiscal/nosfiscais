5395c58e9f72b6083539887031cf1a6bc58264d8
Helder Ribeiro <helder@discor.de>
2014-08-08 22:10:27 -0300

    first commit

---
+++ client/views/locators/locators_list.js
@@ -0,0 +1,20 @@
+var locatorsData = [
+  {
+    name: 'Sudeste'
+  },
+  {
+    name: 'Nordeste'
+  },
+  {
+    name: 'Sul'
+  },
+  {
+    name: 'Centro-Oeste'
+  },
+  {
+    name: 'Norte'
+  }
+];
+Template.locatorsList.helpers({
+  locators: locatorsData
+});
