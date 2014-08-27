b41488a6dc149b222f77158a859644a944c7c9f1
Helder Ribeiro <helder@discor.de>
2014-08-27 19:54:12 -0300

    Replace hardcoded JSON with imported collection data

--- client/views/application/home.js
+++
@@ -1,4 +0,0 @@
-Template.home.helpers({
-  locators: _(['n', 'co', 'ne', 's', 'se']).
-    map(Meteor.myFunctions.fromLocatorSlug)
-});
--- collections/locators.js
+++ collections/locators.js
@@ -1,1 +1,19 @@
-Locators = new Meteor.Collection('locators');
+// A Locator class that takes a document in its constructor
+Locator = function (doc) {
+  _.extend(this, doc);
+};
+
+_.extend(Locator.prototype, {
+  children: function () {
+    return Locators.find({ _id: { $in: this.childrenIds } },
+     { sort: { slug: 1 } });
+  },
+  parent: function () {
+    return Locators.findOne(this.parentId);
+  }
+});
+
+// Define a Collection that uses Locator as its document
+Locators = new Meteor.Collection("locators", {
+  transform: function (doc) { return new Locator(doc); }
+});
