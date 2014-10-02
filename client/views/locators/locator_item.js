8e235e067a37718548589332576f6a579d90c106
Helder Ribeiro <helder@discor.de>
2014-10-02 05:21:56 -0300

    Try to fix reactivity, which is working locally but not in prod

--- client/views/locators/locator_item.js
+++ client/views/locators/locator_item.js
@@ -1,6 +1,6 @@
 Template.locatorItem.helpers({
   emptyZonesStats: function () {
-    var locator = this;
+    var locator = Locators.findOne(this._id);
     var cnt = locator.emptyZonesCount;
     var avg = locator.monitorsCount() / locator.zonesCount;
     return cnt ? 'faltam ' + cnt : parseFloat(Math.round(avg * 10) / 10).toFixed(1) + ' por zona'
