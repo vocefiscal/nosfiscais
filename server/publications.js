7768d52dd54f5cb820fe5cd15ac675b8e237641b
Helder Ribeiro <helder@discor.de>
2014-11-26 11:26:35 -0200

    WIP

--- server/publications.js
+++ server/publications.js
@@ -1,19 +1,19 @@
-Meteor.publish('locatorsFromSlugs', function(slugs) {
+Meteor.publish('locatorsFromSlugs', function (slugs) {
   return Locators.find({ slug: { $in: slugs } },
     { sort: { shortName: 1 } });
 });
 
-Meteor.publish('locatorsFromSlugRegExp', function(strRegExp) {
+Meteor.publish('locatorsFromSlugRegExp', function (strRegExp) {
   return Locators.find({ slug: { $regex: strRegExp } },
     { sort: { shortName: 1 } });
 });
 
-Meteor.publish('locators', function(ids) {
+Meteor.publish('locators', function (ids) {
   return Locators.find({ _id: { $in: ids } },
     { sort: { shortName: 1 } });
 });
 
-Meteor.publish("userData", function () {
+Meteor.publish('userData', function () {
   if (this.userId) {
     return Meteor.users.find({_id: this.userId},
       {fields: {'monitored_locator_ids': 1, 'monitored_locator_slugs': 1,
@@ -21,4 +21,18 @@ Meteor.publish("userData", function () {
   } else {
     this.ready();
   }
+});
+
+Meteor.publish('pollTapeSubmissionToVerify', function (rand) {
+  console.log("rand: " + rand);
+  console.log(PollTapeSubmissions.find().fetch());
+  var result = PollTapeSubmissions.find({ verificationCount: { $lte: 2 },
+    random: { $gte: rand } }, { limit: 1 });
+  if ( result.count() == 0 ) {
+    result = PollTapeSubmissions.find({ verificationCount: { $lte: 2 },
+      random: { $lte: rand } }, { limit: 1 });
+  }
+  // debugger;
+  console.log("result.count(): " + result.count());
+  return result;
 });
